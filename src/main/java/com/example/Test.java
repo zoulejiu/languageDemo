59aa52ae72edfb39ecf28ebffe0a64935754d397
zoulejiu <zoulejiu@qq.com>
2022-03-12 15:34:48 +0800

    init

---
+++ src/main/java/com/example/Test.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("simple:googleUrl1");
+        System.out.println("simple:webUrl1");
+    }
+}
